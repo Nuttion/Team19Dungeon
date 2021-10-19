942bb2a30d1b076afdc52a4e0d9bcd4b4e244e72
Nuttion <flynn.kassulke@bigpond.com>
2021-10-19 14:57:00 +1000

    Yay

---
+++ Assets/Scripts/KeyScript.cs
@@ -0,0 +1,37 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class KeyScript : MonoBehaviour
+{
+    public GameObject Key;
+    public GameObject Door;
+
+    public bool keyCollected = false;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if(collision.gameObject.tag == "Key")
+        {
+            keyCollected = false;
+            Destroy(collision.gameObject);
+        }
+
+        if (collision.gameObject.tag == "EndDoor" && keyCollected == true)
+        {
+            Destroy(collision.gameObject);
+        }
+    }
+}
