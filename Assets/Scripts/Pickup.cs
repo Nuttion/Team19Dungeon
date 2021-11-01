25c6ba72935315f3e886eb274b31a504ffa0de00
FueledByBread <n10790161@qut.edu.au>
2021-11-01 19:45:10 +1000

    yes

--- Assets/Scripts/Pickup.cs
+++ Assets/Scripts/Pickup.cs
@@ -4,17 +4,38 @@ using UnityEngine;
 
 public class Pickup : MonoBehaviour {
 
+    GameObject player;
+    float pHealth;
+
     // Start is called before the first frame update
     void Start() {
+        player = GameObject.FindGameObjectWithTag("Player");
         
     }
 
     // Update is called once per frame
     void Update() {
-        
+        pHealth = player.GetComponent<PlayerAvatar>().health;
     }
 
     public virtual void OnTriggerEnter(Collider other) {
+
+        Debug.Log(pHealth);
+
+        if (gameObject.name == "Health")
+        {
+            if (player.GetComponent<PlayerAvatar>().health < 50)
+            {
+                player.GetComponent<PlayerAvatar>().health += 100 - player.GetComponent<PlayerAvatar>().health;
+            }
+
+            else
+                player.GetComponent<PlayerAvatar>().health += 50;
+            
+
+            //gameObject.SetActive(false);
+        }
+
 
     }
 }
